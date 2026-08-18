428d98031bf588d9e8f5a5b9b5a9b8d46efd0bc5
0Chronic <kyawn1063@gmail.com>
2026-08-19 00:00:44 +0630

    adding backgroung

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Navbar/Hero/Hero'
+
 
 const App = () => {
   return (
   <div>
     <Navbar></Navbar>
+     <Hero></Hero>
   </div>
   )
 }
