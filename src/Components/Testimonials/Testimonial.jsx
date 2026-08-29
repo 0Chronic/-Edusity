c941b2a761bc9ea3e04f88a9562e487896d2de83
0Chronic <kyawn1063@gmail.com>
2026-08-29 22:53:47 +0630

    really slide

--- src/Components/Testimonials/Testimonial.jsx
+++ src/Components/Testimonials/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useRef} from 'react'
 import './Testimonial.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
@@ -11,14 +11,22 @@ import user_4 from '../../assets/user-4.png'
 
 const Testimonial = () => {
 
-
+  const slider = useRef();
+  let tx=0;
 
   const slideForward = ()=>{
-
+    if(tx > -50){
+      tx -= 25
+    }
+    slider.current.style.transform = `translateX(${tx}%)`
   }
 
 
    const slideBackward = ()=>{
+      if(tx < 0){
+      tx += 25
+    }
+    slider.current.style.transform = `translateX(${tx}%)`
     
   }
 
@@ -29,8 +37,7 @@ const Testimonial = () => {
         <img src={next_icon} className='next-btn' onClick={slideForward} alt="" />
          <img src={back_icon} className='back-btn' onClick={slideBackward} alt="" />
          <div className="slider">
-            <ul>
-
+            <ul ref={slider}>
 
               <li>
                 <div className="slide">
